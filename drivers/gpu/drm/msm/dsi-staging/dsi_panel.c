1456f875b8edeb5341c464b2cbffbda01b558f15
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-05-28 18:30:58 +0530

    drm: msm: expose HBM state
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -3766,6 +3766,7 @@ static int dsi_panel_parse_mi_config(struct dsi_panel *panel,
 
 	dsi_panel_parse_elvss_dimming_config(panel);
 
+	panel->hbm_enabled = false;
 	panel->fod_hbm_enabled = false;
 	panel->fod_dimlayer_hbm_enabled = false;
 	panel->skip_dimmingon = STATE_NONE;
@@ -5151,6 +5152,7 @@ static int panel_disp_param_send_lock(struct dsi_panel *panel, int param)
 		pr_info("hbm on\n");
 		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_HBM_ON);
 		panel->skip_dimmingon = STATE_DIM_BLOCK;
+		panel->hbm_enabled = true;
 		break;
 	case DISPPARAM_HBM_FOD_ON:
 		pr_info("hbm fod on\n");
@@ -5233,6 +5235,7 @@ static int panel_disp_param_send_lock(struct dsi_panel *panel, int param)
 		pr_info("hbm off\n");
 		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_HBM_OFF);
 		panel->skip_dimmingon = STATE_DIM_RESTORE;
+		panel->hbm_enabled = false;
 		break;
 	default:
 		break;
@@ -5568,6 +5571,7 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	else
 		panel->panel_initialized = true;
 
+	panel->hbm_enabled = false;
 	panel->fod_hbm_enabled = false;
 	panel->fod_dimlayer_hbm_enabled = false;
 	panel->in_aod = false;
@@ -5667,6 +5671,7 @@ int dsi_panel_disable(struct dsi_panel *panel)
 
 	panel->panel_initialized = false;
 	panel->skip_dimmingon = STATE_NONE;
+	panel->hbm_enabled = false;
 	panel->fod_hbm_enabled = false;
 	panel->fod_dimlayer_hbm_enabled = false;
 	panel->in_aod = false;
